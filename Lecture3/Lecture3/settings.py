e3ebc7d697ce3911e5e694f41e81d49ed05509eb
zaimeali <zaimeali1997@gmail.com>
2021-04-09 13:58:32 -0400

    Is it NewYear App Done

--- Lecture3/Lecture3/settings.py
+++ Lecture3/Lecture3/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'hello',    # initialize app
+    'newyear',  # initialize app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ Lecture3/newyear/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import datetime
+
+# Create your views here.
+
+
+def index(request):
+    # return HttpResponse('nice')
+    now = datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
